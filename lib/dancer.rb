e355efaee1d4e353c18dc0d159086244ef4db0ba
Raul Sposito <raulsposito@gmail.com>
2019-10-23 23:13:35 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,16 +2,13 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 
-class Dancer 
-
+class Dancer
   include Dance 
-  
   attr_accessor :name 
   
   def initialize(name)
     @name = name 
   end
-  
 end
 
 class Dancer
