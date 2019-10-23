c850c339cc858c09911dc9310eb33a8b955864e4
Raul Sposito <raulsposito@gmail.com>
2019-10-23 23:07:36 +0000

    Automatically backed up by Learn

--- lib/fancy_dance.rb
+++ lib/fancy_dance.rb
@@ -1,5 +1,11 @@
 class Dancer 
-  extend FancyDance::ClassMEthods
+  extend FancyDance::ClassMethods
   include 
   FancyDance::InstanceMethods
+end
+
+class Kid 
+  extend FancyDance::ClassMethods 
+  include 
+  FancyDance::InstanceMEthods 
 end
